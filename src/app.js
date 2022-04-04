c35fb9a47d9911ed965a14e0c4f2c48150a35c49
Guilherme Leon <guilhermeleon2012@hotmail.com>
2022-04-04 11:48:59 -0300

    att

--- src/app.js
+++ src/app.js
@@ -1,11 +1,11 @@
 import express from "express";
 
 const app = express();
-
+app.use(express.json());    //Ao utilizar o push ou post para interpretar oq foi recebido
 
 const sensores=[
     {id: 1, "titulo": "Sensor de temperatura"},
-    {id: 2, "titulo": "Sensor de pressão"}
+    {id: 2, "titulo": "Sensor de pressão"},
 ]
 
 app.get('/', (req, res) => {
@@ -21,8 +21,25 @@ app.get('/sensores/:id', (req, res) => {
     res.json(sensores[index]);
 })
 
+app.post('/sensores', (req, res) => {
+    sensores.push(req.body);
+    res.status(201).send('O sensor foi cadastrado com sucesso')
+})
+
+app.put('/sensdores/:id', (req, res) => {
+    let index = buscarSensores(req.params.id);
+    sensores[index].titulo = req.body.titulo;
+    res.json(sensores);
+  })
+
+app.delete('/sensdores/:id', (req, res) => {
+    let index = buscarSensores(req.params.id);
+    sensores.splice(index, 1);
+    res.send(`O sensor ${id} foi removido com sucesso`);
+})
+
 function buscarSensores(id) {
-    return sensores.findIndex(livro => livro.id == id)
+    return sensores.findIndex(sensor => sensor.id == id)
   }
 
   export default app;
